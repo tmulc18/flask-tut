57f9e3ec9c69c3c551d3d85a0e353ec3311b1621
deepbox <tmulc18@gmail.com>
2019-12-08 19:23:22 -0800

    added user profile (Chapter 6)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
@@ -14,6 +15,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,6 +27,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
